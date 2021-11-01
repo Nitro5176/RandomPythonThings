de33beeac877a18d528571e603e812242238061a
Stephen <st.98.30.03@gmail.com>
2021-11-01 09:21:55 -0400

    Added small changes

--- MonitorNetwork.py
+++ MonitorNetwork.py
@@ -1,13 +1,35 @@
-import speedtest_cli as spd
-
+import speedtest as spd
 
 
 def upload():
-    return spd.SpeedTest().upload()
+    return spd.Speedtest().upload()/1000000
+
 def download():
+    return spd.Speedtest().download()/1000000
 
 def ping():
-    return spd
-
+    pingms = spd.Speedtest().get_best_server()
+    return pingms['latency']
 
 print(upload())
+print(download())
+print(ping())
+
+#
+# servers = []
+# # If you want to test against a specific server
+# # servers = [1234]
+#
+# threads = None
+# # If you want to use a single threaded test
+# # threads = 1
+# #
+# # s = spd.Speedtest()
+# # s.get_servers(servers)
+# # s.get_best_server()
+# # s.download(threads=threads)
+# # s.upload(threads=threads)
+# # s.results.share()
+# #
+# # results_dict = s.results.dict()
+
